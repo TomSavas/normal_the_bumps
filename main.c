35ab9ad290d06d1b71d57392e25f94761bf3d098
TomSavas <tomas.savickas@protonmail.com>
2021-05-05 22:35:02 +0300

    Bugfix

--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-inline int min(int a, int b)
+int min(int a, int b)
 {
     return a < b ? a : b;
 }
 
-inline int max(int a, int b)
+int max(int a, int b)
 {
     return a > b ? a : b;
 }
@@ -37,25 +37,25 @@ int main(int argc, const char **argv)
     unsigned char *normal_img = malloc(sizeof(unsigned char)*3 * x * y);
     for (int i = 0; i < y; i++)
     {
-        int row_idx = i * y;
-        int top_idx = min(max((i-1), 0), y) * y;
-        int bot_idx = min(max((i+1), 0), y) * y;
+        int row_idx = i * x;
+        int top_idx = min(max((i-1), 0), y) * x;
+        int bot_idx = min(max((i+1), 0), y) * x;
 
         for (int j = 0; j < x; j++)
         {
-            int left_ind = min(max((j-1), 0), x);
-            int right_ind = min(max((j+1), 0), x);
+            int left_idx = min(max((j-1), 0), x);
+            int right_idx = min(max((j+1), 0), x);
 
-            float top_left  = img[top_idx + left_ind];
+            float top_left  = img[top_idx + left_idx];
             float top       = img[top_idx + j];
-            float top_right = img[top_idx + right_ind];
+            float top_right = img[top_idx + right_idx];
 
-            float left      = img[row_idx + left_ind];
-            float right     = img[row_idx + right_ind];
+            float left      = img[row_idx + left_idx];
+            float right     = img[row_idx + right_idx];
 
-            float bot_left  = img[bot_idx + left_ind];
+            float bot_left  = img[bot_idx + left_idx];
             float bot       = img[bot_idx + j];
-            float bot_right = img[bot_idx + right_ind];
+            float bot_right = img[bot_idx + right_idx];
  
             // sobel kernel
             float dX = -(top_left + 2*left + bot_left ) + (top_right + 2*right + bot_right);
@@ -67,7 +67,7 @@ int main(int argc, const char **argv)
             float vec_y = dY / len;
             float vec_z = softness / len;
 
-            int index = (i*y + j);
+            int index = (i*x + j);
             normal_img[index*3 + 0] = (vec_x+1) * 255.0/2.0;
             normal_img[index*3 + 1] = (vec_y+1) * 255.0/2.0;
             normal_img[index*3 + 2] = (vec_z+1) * 255.0/2.0;

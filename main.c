6c726b3b77da2b7c101cb31563f7d506f32b48a4
TomSavas <tomas.savickas@protonmail.com>
2021-05-05 21:18:41 +0300

    Add implementation

---
+++ main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <math.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+inline int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+inline int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int main(int argc, const char **argv)
+{
+    if (argc < 2)
+    {
+        printf("No file provided.\n");
+        return 1;
+    }
+
+    const char *out_file = "out_normal.jpg";
+    if (argc > 2)
+        out_file = argv[2];
+
+    const float softness = 32.f;
+    const float sqr_softness = softness * softness;
+
+    int x, y, n;
+    unsigned char *img = stbi_load(argv[1], &x, &y, &n, 1);
+
+    unsigned char *normal_img = malloc(sizeof(unsigned char)*3 * x * y);
+    for (int i = 0; i < y; i++)
+    {
+        int row_idx = i * y;
+        int top_idx = min(max((i-1), 0), y) * y;
+        int bot_idx = min(max((i+1), 0), y) * y;
+
+        for (int j = 0; j < x; j++)
+        {
+            int left_ind = min(max((j-1), 0), x);
+            int right_ind = min(max((j+1), 0), x);
+
+            float top_left  = img[top_idx + left_ind];
+            float top       = img[top_idx + j];
+            float top_right = img[top_idx + right_ind];
+
+            float left      = img[row_idx + left_ind];
+            float right     = img[row_idx + right_ind];
+
+            float bot_left  = img[bot_idx + left_ind];
+            float bot       = img[bot_idx + j];
+            float bot_right = img[bot_idx + right_ind];
+ 
+            // sobel kernel
+            float dX = -(top_left + 2*left + bot_left ) + (top_right + 2*right + bot_right);
+            float dY = -(top_left + 2*top  + top_right) + (bot_left  + 2*bot   + bot_right);
+
+            // normalize
+            float len = sqrt(dX*dX + dY*dY + sqr_softness);
+            float vec_x = dX / len;
+            float vec_y = dY / len;
+            float vec_z = softness / len;
+
+            int index = (i*y + j);
+            normal_img[index*3 + 0] = (vec_x+1) * 255.0/2.0;
+            normal_img[index*3 + 1] = (vec_y+1) * 255.0/2.0;
+            normal_img[index*3 + 2] = (vec_z+1) * 255.0/2.0;
+        }
+    }
+
+    stbi_write_jpg(out_file, x, y, 3, normal_img, 100);
+
+    stbi_image_free(img);
+    free(normal_img);
+
+    return 0;
+}
